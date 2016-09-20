04e9b0e5a2f42793e6e5a89c1ce63f870ff76075
Patrick DeWitte <patrickhdewitte@gmail.com>
2016-09-20 12:16:58 -0500

    Hide API keys

--- app/helpers/spotify_caller.rb
+++ app/helpers/spotify_caller.rb
@@ -8,7 +8,8 @@ module SpotifyCaller
   end
 
   def self.spotify_authorize
-    encoded_client = Base64.encode64("12f031a61c9a4fbe99af563a5d600b64:8a1e2faa3a534bd0a930616d7e592a3d").delete("\n")
+    p ENV["SPOTIFY_CLIENT_SECRET"]
+    encoded_client = Base64.encode64(SpotifyCaller.client_id + ":" + SpotifyCaller.client_secret).delete("\n")
 
     HTTParty.post("https://accounts.spotify.com/api/token",
       :query => { :grant_type => "client_credentials" },
